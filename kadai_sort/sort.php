ead7611bdf34bad4c9095a4be89b8346e11dc304
nanaminmi07 <newslove.nana@me.com>
2026-02-14 16:36:44 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,26 +9,32 @@
 <body>
     <p>
         <?php
-        function sort_2way($array, $order){
-            $array = [15,4,18,23,10];
-            if($order){
-                echo "昇順にしました<br>";
-                sort($array);
-                foreach($array as $num){
-                echo $num . '<br>';    
-                }
-                    
-            }else {
-                echo "降順にしました<br>";
-                rsort($array);
-                foreach($array as $num){
-                echo $num . '<br>';    
-                }
-            }
-            }      
-    sort_2way($array,true);
-    sort_2way($array,false);
-        ?>
+// ① 配列を関数の外側で1度だけ宣言
+$nums = [15, 4, 18, 23, 10];
+
+function sort_2way($array, $order) {
+    // ② 渡された配列をそのまま並び替え
+    if ($order) {
+        echo "昇順にしました<br>";
+        $sorted = $array; // 配列をコピー
+        sort($sorted);    // コピーした配列を並び替え
+        foreach ($sorted as $num) {
+            echo $num . '<br>';
+        }
+    } else {
+        echo "降順にしました<br>";
+        $sorted = $array;
+        rsort($sorted);
+        foreach ($sorted as $num) {
+            echo $num . '<br>';
+        }
+    }
+}
+
+// ③ 配列とtrue/falseを引数として渡す
+sort_2way($nums, true);   // 昇順
+sort_2way($nums, false);  // 降順
+?>
     </p>
 </body>
 
